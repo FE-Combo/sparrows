6287a607eb7e345fa736a33f9bc3f66e168435de
vocoWone <605131105@qq.com>
2023-06-19 18:27:42 +0800

    feat: add cluster version for redis middleware

--- src/middlewares/redis.ts
+++ src/middlewares/redis.ts
@@ -8,18 +8,27 @@ export interface RedisCTX extends DefaultContext {
   redis: KoaRedis;
 }
 
-export interface KoaRedis extends Options {
-  redis: Redis.Redis;
+export interface KoaRedis extends Options, Omit<Partial<ClusterOptions>, "sessionOptions"> {
+  redis: Redis.Redis | Redis.Cluster;
   redlock: RedLock;
 }
 
 export interface Options {
-  redisOptions: Redis.RedisOptions;
+  redisOptions?: Redis.RedisOptions;
+  sessionOptions: SessionOptions;
+  redlockSettings?: Settings;
+}
+
+export interface ClusterOptions {
+  clusterNodes: Redis.ClusterNode[];
+  clusterOptions?: Redis.ClusterOptions
   sessionOptions: SessionOptions;
   redlockSettings?: Settings;
 }
 
 let redis: Redis.Redis | null = null;
+
+let clusterRedis: Redis.Cluster | null = null;
 
 let redlock: RedLock | null = null;
 
@@ -42,14 +51,40 @@ export const middleware =
       redlockSettings,
     };
     await session({
-      store: redisStore(
-        (redisOptions || {}) as redisStore.RedisOptions
-      ) as unknown as SessionStore,
+      store: redisStore({client: redis}) as unknown as SessionStore,
       ...sessionOptions,
     })(ctx, next);
   };
 
-// 该方法会返回请求头 set-cookie（在浏览器种下cookie），返回的sessionId就是对应浏览器的cookie值。但是当前链路不会立即生效需要发送到客户端后才能生效，所以ctx.cookies.get(sid)拿不到最新值，若在同一链路上更新cookie需要手动将sid存储到ctx上或往下透传
+  // 集群版
+  export const clusterMiddleware =
+  (options: ClusterOptions) =>
+  async (ctx: ParameterizedContext<DefaultState, RedisCTX>, next: Next) => {
+    const { clusterNodes, clusterOptions, redlockSettings, sessionOptions } = options;
+    if (!clusterRedis) {
+      clusterRedis = new Redis.Cluster(clusterNodes, clusterOptions);
+    }
+    if (!redlock) {
+      redlock = new RedLock([clusterRedis], redlockSettings || {});
+    }
+
+    ctx.redis = {
+      redis: clusterRedis,
+      redlock,
+      clusterNodes,
+      clusterOptions,
+      sessionOptions,
+      redlockSettings,
+    };
+    await session({
+      store: redisStore({
+        client: clusterRedis
+      }) as unknown as SessionStore,
+      ...sessionOptions,
+    })(ctx, next);
+  };
+
+// 该方法会返回请求头 set-cookie（在浏览器种下cookie），返回的sessionId就是对应浏览器的cookie值。但是当前链路不会立即生效需要发送到客户端后才能生效，所以ctx.cookies.get(sid)拿不到最新值，若在同一链路上更新cookie需要手动将sid存储到ctx上或往下透传。该方案虽然解决了服务端问题但是客户端同时发起多个并发请求时（Promise all）会导致部分接口使用旧的 cookie，所以需要在业务测加入锁的逻辑
 export const saveSession = async (
   // eslint-disable-next-line
   value: any,
